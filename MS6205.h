d9d335f3661e39870c80eb36d4609268ed704f98
Christian Holzapfel <accounts@holzapfel.biz>
2018-12-07 17:18:00 +0100

    Added cyrillic characters to code map
    Signed-off-by: Christian Holzapfel <accounts@holzapfel.biz>

--- MS6205.h
+++ MS6205.h
@@ -49,7 +49,7 @@
    Character  @  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z  [  \  ]  ^  _                                    
                                                                                                                                                 
    Code       96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127       
-   Character  <cyrillic characters>
+   Character  Ю  А  Б  Ц  Д   Е   Ф   Г   Х   И   Й   К   Л   М   Н   О   П   Я   Р   С   Т   У   Ж   В   Ь   Ы   З   Ш   Э   Щ   Ч   █
    
    
   CURSOR (optional)
